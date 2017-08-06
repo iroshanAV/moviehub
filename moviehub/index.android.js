2ac95209e306d2c9551bb96d7a9eeb3387c4bee1
Iroshan Vithanage <iroshanav@gmail.com>
2017-08-06 17:42:03 +0530

    removed comments

--- moviehub/index.android.js
+++ moviehub/index.android.js
@@ -1,14 +1,13 @@
-//import library to help the component
+
 import React from 'react';
 import {AppRegistry,View} from 'react-native';
 import Header from './src/components/Header';
 import MovieList from './src/components/MovieList';
 
-//create a component
 
 
-//root component walin child component ekata monawahari 
-//pass karanna one nam api use karanawa 'props' 
+
+
 const App = () => {
  
   return(
@@ -20,8 +19,4 @@ const App = () => {
 }
 
 
-
-
-
-//Render it to the device
 AppRegistry.registerComponent('moviehub',()=>App);
